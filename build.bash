246174c1135674a1ea233f4d7efb2252cc8469c0
Dmitry Bushev <bushevdv@gmail.com>
2015-05-20 12:43:42 +0300

    hadoop 2.6.0

--- build.bash
+++ build.bash
@@ -3,14 +3,14 @@
 set -e
 
 # build protobuf
-cd $PROTOBUF_SOURCES && ./configure && make
+cd "$PROTOBUF_SOURCES" && ./configure && make
 
 # build hadoop
 export HADOOP_PROTOC_PATH=$PROTOBUF_SOURCES/src/protoc
-cd $HADOOP_SOURCES && mvn package -Pdist,native -DskipTests -Dtar
-mv $HADOOP_SOURCES/hadoop-dist/target/hadoop-2.5.2.tar.gz /opt/
+cd "$HADOOP_SOURCES" && mvn package -Pdist,native -DskipTests -Dtar
+mv "$HADOOP_SOURCES"/hadoop-dist/target/hadoop-2.6.0.tar.gz /opt/
 
 # cleanup
-cd $PROTOBUF_SOURCES && make clean
-cd $HADOOP_SOURCES && mvn clean
-rm -rf $HOME/.m2
+cd "$PROTOBUF_SOURCES" && make clean
+cd "$HADOOP_SOURCES" && mvn clean
+rm -rf "$HOME"/.m2
